33e58c7375c5a6bc3ae487f7c173741be77da9f3
Flavio Gouvea <flaviogouveaneto@gmail.com>
2023-08-01 23:46:26 -0300

    add Assembly_3.sol

---
+++ 05-Assembly1/assembly/Assembly_3.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.4;
+
+contract SubOverflow {
+    // Modify this function so that on overflow it returns the value 0
+    // otherwise it should return x - y
+    function subtract(uint256 x, uint256 y) public pure returns (uint256) {
+        // Write assembly code that handles overflows
+        assembly {
+            if lt(y,x) {
+                let result := sub(x,y)
+                mstore(0x1, result)
+                return(0x1, 32)
+            }
+            return (0x2, 32)
+        }
+    }
+}
